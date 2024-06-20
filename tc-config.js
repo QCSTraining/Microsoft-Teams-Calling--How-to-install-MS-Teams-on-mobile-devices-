0431e5b9a6c3f050c803d1509cca500240666185
QCSTraining <fantasticfourlegal@gmail.com>
2024-06-20 10:07:39 +1000

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://LGl-X1UnJOghULe3ml36PwkjWrJM9KLV_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "A Quick Guide to Install Microsoft Teams on Mobile Devices"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
